35fde933455cdfa9bb93eed44a73d965755224bd
eman <eman@gmail.com>
2025-04-22 21:19:07 +0200

    ADD

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -20,7 +20,7 @@ function Project() {
 
       <div className='grid md:grid-cols-2  items-center grid-cols-1'>
           
-          <div>
+          <div className='px-[30px]'>
           <img src="/Portofolio/Screenshot (159).png" className='w-[400px] rounded-2xl hover:scale-105 -100 my-[30px]  transition-transform duration-300'/>
 
           </div>
@@ -32,7 +32,7 @@ function Project() {
           </p>
           <br/>
           <br/>
-          <a href="https://emanosama9.github.io/menue/ " className='text-white ' target="_blank" ><h3 className='border text-center border-purple-600  py-2'>GitHub: https://emanosama9.github.io/menue/</h3> </a>
+          <a href="https://emanosama9.github.io/menue/ " className='text-white ' target="_blank" ><h3 className='border text-center border-purple-600 w-[280px] md:px-2 py-2'>GitHub: https://emanosama9.github.io/menue/</h3> </a>
 
              </div>
         </div>
@@ -55,7 +55,7 @@ function Project() {
  <div className= 'grid md:grid-cols-2  items-center  grid-cols-1 '>
 
  
-<div className='px-8'>
+<div>
 <h3 className='text-center'>  E-commerce-App</h3>
 
         <p> A platform that displays products and allows users to add them to the cart and filter products.</p>
@@ -64,7 +64,7 @@ function Project() {
 
        <a href="https://emanosama9.github.io/e-commerce/ " className='text-white ' target="_blank" ><h3 className='border text-center  border-purple-600 px-2 py-2'>GitHub: https://emanosama9.github.io/e-commerce/</h3> </a>
 </div>
-    <div>
+    <div className='px-[30px]'>
       
            <img src="/Portofolio/Screenshot (70).png" className='w-[400px] rounded-2xl hover:scale-105  my-[30px] transition-transform duration-300'/>
 
@@ -86,7 +86,7 @@ function Project() {
 
     <div  className='grid md:grid-cols-2  items-center  grid-cols-1'>
           
-          <div>
+          <div className='gap-11 px-[30px]'>
               <img src="/Portofolio/Screenshot (120).png" className='w-[400px] rounded-2xl hover:scale-105  my-[30px] transition-transform duration-300'/>
 
             </div> 
@@ -96,7 +96,7 @@ function Project() {
         <p > An app that displays the Quran's surahs and verses in an elegant as well as the supplications.  manner  using an API . </p>
                   <br/>
                   <br/>
-                  <a href="https://emanosama9.github.io/quran/" className='text-white ' target="_blank" ><h3 className='border border-purple-600  text-center px-2 py-2'>GitHub: https://emanosama9.github.io/todolist/</h3> </a>
+                  <a href="https://emanosama9.github.io/quran/" className='text-white ' target="_blank" ><h3 className='border border-purple-600  text-center px-2 py-2'>GitHub: https://emanosama9.github.io/quran/</h3> </a>
 
               </div>
     </div>
@@ -116,15 +116,15 @@ function Project() {
  <div className= 'grid md:grid-cols-2 text-center grid-cols-1 items-center '>
 
  
-<div className='px-8'>
+<div>
 <h3>TodoList</h3>
         <p>An interactive task management app where can add, delete, and update tasks. users  </p>
            <br/>
          <br/>
-         <a href="https://emanosama9.github.io/todolist/" className='text-white ' target="_blank" ><h3 className='border border-purple-600  px-2 py-2'>GitHub: https://emanosama9.github.io/todolist/</h3> </a>
+         <a href="https://emanosama9.github.io/todolist/" className='text-white ' target="_blank" ><h3 className='border border-purple-600 px-5 md:px-2 md:py-2'>GitHub: https://emanosama9.github.io/todolist/</h3> </a>
 
-</div>
-<div>
+</div >
+<div className='px-[30px]'>
     <img src="/Portofolio/Screenshot (64).png" className='w-[400px] rounded-2xl hover:scale-105   my-[30px] transition-transform duration-300'/>
 
    </div> 
